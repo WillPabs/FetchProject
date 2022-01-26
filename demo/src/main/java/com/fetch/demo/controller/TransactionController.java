4ee084905777fcaf5e517fd2f7623e2d2037cc76
Will Pabs <pabiterowilliam@yahoo.com>
2022-01-26 17:07:12 -0500

    Fixed spendPoints and addTransaction method

--- demo/src/main/java/com/fetch/demo/controller/TransactionController.java
+++ demo/src/main/java/com/fetch/demo/controller/TransactionController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import com.fetch.demo.entity.SpentTransaction;
 import com.fetch.demo.entity.Transaction;
 
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class TransactionController {
 
     private final List<Transaction> transactions;
+    private HashMap<String, Integer> payersPoints = new HashMap<>();
     
     private TransactionController(List<Transaction> transactions) {
         this.transactions = transactions;
@@ -25,7 +27,7 @@ public class TransactionController {
     }
 
     @GetMapping("/transactions/{name}")
-    public List<Transaction> getTransactions(@PathVariable("name") String name) {
+    public List<Transaction> getTransactionsByName(@PathVariable("name") String name) {
         try {
             List<Transaction> payerTransactions = new ArrayList<>();
 
@@ -44,39 +46,54 @@ public class TransactionController {
 
     @GetMapping("/transactions/points")
     public HashMap<String, Integer> getPayersPoints() {
-        HashMap<String, Integer> payersPoints = new HashMap<>();
+        return this.payersPoints;
+    }
 
-        // check if payer exists in hashmap, add to map; else add current txn points to existing points
-        for (Transaction txn : transactions) {
+    @PostMapping("/transactions/spend")
+    public List<SpentTransaction> spendPoints(@RequestBody Transaction spendPoints) {
+        this.transactions.sort((txn1, txn2) -> txn1.getDate().compareTo(txn2.getDate()));
+        List<Transaction> txnList = new ArrayList<>(this.transactions); //copy of transactions to avoid ConcurrentModificationException
+        
+        List<SpentTransaction> spentTransactions = new ArrayList<>();
+        int spent = spendPoints.getPoints();
+        // loop through transactions, subtract points from payer's points
+        for (Transaction txn : txnList) {
+            if (spent > 0) {
+                if (spent - txn.getPoints() < 0) {
+                    spent = txn.getPoints() - spent;
+                    spentTransactions.add(new SpentTransaction(txn.getPayer(), spent * -1));
+                    this.payersPoints.put(txn.getPayer(), spent);
+                    txn.setPoints(spent);
+                    spent = 0;
+                } else {
+                    spent -= txn.getPoints();
+                    spentTransactions.add(new SpentTransaction(txn.getPayer(), txn.getPoints() * -1));
+                    this.payersPoints.put(txn.getPayer(), this.payersPoints.get(txn.getPayer()) - txn.getPoints());
+                    txn.setPoints(0);
+                    // this.transactions.remove(txn);
+                }
+            } else {
+                break;
+            }
+        }
+
+        return spentTransactions;        
+    }
+
+    @PostMapping("/transactions")
+    public ResponseEntity<?> addTransaction(@RequestBody Transaction newTransaction) {
+        Transaction txn = new Transaction(newTransaction.getPayer(), newTransaction.getPoints());
+        try {
             if (payersPoints.get(txn.getPayer()) == null) {
                 payersPoints.put(txn.getPayer(), Integer.valueOf(txn.getPoints()));
             } else {
                 int points = payersPoints.get(txn.getPayer()) + txn.getPoints();
                 payersPoints.put(txn.getPayer(), Integer.valueOf(points));
             }
+            transactions.add(txn);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Unsuccessful attempt to add transaction", HttpStatus.NOT_FOUND); 
         }
-
-        return payersPoints;
-    }
-
-    @GetMapping("/transactions/spend")
-    public List<Transaction> spendPoints(@RequestParam int points) {
-        
-        transactions.sort((txn1, txn2) -> txn1.getDate().compareTo(txn2.getDate()));
-        return transactions;
-
-        // Collections.sort(transactions, new Comparator<Transaction>() {
-        //     public int compare(Transaction txn1, Transaction txn2) {
-        //         return txn1.getDate().compareTo(txn2.getDate());
-        //     }
-        // });
-        
-    }
-
-    @PostMapping("/transactions")
-    public ResponseEntity<?> addTransaction(@RequestBody Transaction newTransaction) {
-        Transaction txn = new Transaction(newTransaction.getPayer(), newTransaction.getPoints());
-        transactions.add(txn);
         return new ResponseEntity<>("Successfully added transaction", HttpStatus.CREATED);
     }
     
