d85520c439f75b51268bbf7c256881c3977d7db4
Will Pabs <pabiterowilliam@yahoo.com>
2022-01-31 15:28:19 -0500

    Added new test method

--- demo/src/test/java/com/fetch/demo/WebMockTest.java
+++ demo/src/test/java/com/fetch/demo/WebMockTest.java
@@ -2,6 +2,7 @@ package com.fetch.demo;
 
 import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
@@ -14,6 +15,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fetch.demo.controller.TransactionController;
 import com.fetch.demo.entity.Transaction;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -38,13 +40,13 @@ public class WebMockTest {
 
     private List<Transaction> txnList = new ArrayList<>();
 
-//    @Before
-//    public void setUp() {
-//        txnList.add(new Transaction("Will", 1000));
-//        txnList.add(new Transaction("Jack", 1000));
-//        txnList.add(new Transaction("John", 1000));
-//        txnList.add(new Transaction("Mark", 1000));
-//    }
+   @Before
+   public void setUp() {
+       txnList.add(new Transaction("Will", 1000));
+       txnList.add(new Transaction("Jack", 1000));
+       txnList.add(new Transaction("John", 1000));
+       txnList.add(new Transaction("Mark", 1000));
+   }
 
 
 
@@ -55,24 +57,38 @@ public class WebMockTest {
     @Test
     public void methodShouldReturnListOfTransactions() {
         
-
-        // Then
         try {
-            txnList.add(new Transaction("Will", 1000));
-            txnList.add(new Transaction("Jack", 1000));
-            txnList.add(new Transaction("John", 1000));
-            txnList.add(new Transaction("Mark", 1000));
             when(txnController.getTransactions())
                 .thenReturn(txnList);
+
             this.mockMvc.perform(get("/transactions"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath("$", hasSize(4)))
-                .andExpect((ResultMatcher) jsonPath("$[0].payer", is("Will")));
-
+                .andExpect(jsonPath("$[*]", hasSize(4)))
+                .andExpect(jsonPath("$[0].payer").value("Will"))
+                .andExpect(jsonPath("$[0].points").value(1000))
+                .andExpect(jsonPath("$[1].payer").value("Jack"))
+                .andExpect(jsonPath("$[1].points").value(1000))
+                .andExpect(jsonPath("$[2].payer").value("John"))
+                .andExpect(jsonPath("$[2].points").value(1000))
+                .andExpect(jsonPath("$[3].payer").value("Mark"))
+                .andExpect(jsonPath("$[3].points").value(1000));
+            
+            verify(txnController, times(1)).getTransactions();
         } catch (Exception e) {
-            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void assertThatGetTransactionsReturnsListOfTransactions() {
+        objectMapper = new ObjectMapper();
+        try {
+            String txnString = objectMapper.writeValueAsString(txnList);
+
+            assert(txnString).equals(txnList.toString());
+        } catch(Exception e) {
             e.printStackTrace();
         }
     }
