1aaefc1175b334931ad87541d99276b685abb374
Will <pabiterowilliam@yahoo.com>
2022-02-01 18:01:08 -0500

    Abstracted controller logic to service

--- demo/src/main/java/com/fetch/demo/controller/TransactionController.java
+++ demo/src/main/java/com/fetch/demo/controller/TransactionController.java
@@ -7,8 +7,8 @@ import java.util.List;
 import com.fetch.demo.entity.SpentTransaction;
 import com.fetch.demo.entity.Transaction;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import com.fetch.demo.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,14 +16,17 @@ public class TransactionController {
 
     private final List<Transaction> transactions;
     private HashMap<String, Integer> payersPoints = new HashMap<>();
-    
+
+    @Autowired
+    private TransactionService transactionService;
+
     private TransactionController(List<Transaction> transactions) {
         this.transactions = transactions;
     }
-    
+
     @GetMapping("/transactions")
     public List<Transaction> getTransactions() {
-        return this.transactions;
+        return transactionService.findTransactions();
     }
 
     @GetMapping("/transactions/{name}")
@@ -46,55 +49,17 @@ public class TransactionController {
 
     @GetMapping("/transactions/points")
     public HashMap<String, Integer> getPayersPoints() {
-        return this.payersPoints;
+        return transactionService.findPayersPoints();
     }
 
     @PostMapping("/transactions/spend")
-    public List<SpentTransaction> spendPoints(@RequestBody Transaction spendPoints) {
-        this.transactions.sort((txn1, txn2) -> txn1.getDate().compareTo(txn2.getDate()));
-        List<Transaction> txnList = new ArrayList<>(this.transactions); //copy of transactions to avoid ConcurrentModificationException
-        
-        List<SpentTransaction> spentTransactions = new ArrayList<>();
-        int spent = spendPoints.getPoints();
-        // loop through transactions, subtract points from payer's points
-        for (Transaction txn : txnList) {
-            if (spent > 0) {
-                if (spent - txn.getPoints() < 0) {
-                    spent = txn.getPoints() - spent;
-                    spentTransactions.add(new SpentTransaction(txn.getPayer(), spent * -1));
-                    this.payersPoints.put(txn.getPayer(), spent);
-                    txn.setPoints(spent);
-                    spent = 0;
-                } else {
-                    spent -= txn.getPoints();
-                    spentTransactions.add(new SpentTransaction(txn.getPayer(), txn.getPoints() * -1));
-                    this.payersPoints.put(txn.getPayer(), this.payersPoints.get(txn.getPayer()) - txn.getPoints());
-                    txn.setPoints(0);
-                    // this.transactions.remove(txn);
-                }
-            } else {
-                break;
-            }
-        }
-
-        return spentTransactions;        
+    public List<SpentTransaction> spendPoints(@RequestBody Transaction pointsToSpend) {
+        return transactionService.spendPoints(pointsToSpend);
     }
 
     @PostMapping("/transactions")
-    public ResponseEntity<?> addTransaction(@RequestBody Transaction newTransaction) {
-        Transaction txn = new Transaction(newTransaction.getPayer(), newTransaction.getPoints());
-        try {
-            if (payersPoints.get(txn.getPayer()) == null) {
-                payersPoints.put(txn.getPayer(), Integer.valueOf(txn.getPoints()));
-            } else {
-                int points = payersPoints.get(txn.getPayer()) + txn.getPoints();
-                payersPoints.put(txn.getPayer(), Integer.valueOf(points));
-            }
-            transactions.add(txn);
-        } catch (Exception e) {
-            return new ResponseEntity<>("Unsuccessful attempt to add transaction", HttpStatus.NOT_FOUND); 
-        }
-        return new ResponseEntity<>("Successfully added transaction", HttpStatus.CREATED);
+    public Transaction addTransaction(@RequestBody Transaction newTransaction) {
+        return transactionService.saveTransaction(newTransaction);
     }
-    
+
 }
---
+++ demo/src/main/java/com/fetch/demo/service/TransactionService.java
@@ -0,0 +1,76 @@
+package com.fetch.demo.service;
+
+import com.fetch.demo.entity.SpentTransaction;
+import com.fetch.demo.entity.Transaction;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+public class TransactionService {
+
+    private static List<Transaction> transactions;
+    private HashMap<String, Integer> payersPoints = new HashMap<>();
+
+    private TransactionService (List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    public Transaction saveTransaction(Transaction transaction) {
+        Transaction txn = new Transaction(transaction.getPayer(), transaction.getPoints());
+        try {
+            if (payersPoints.get(txn.getPayer()) == null) {
+                payersPoints.put(txn.getPayer(), Integer.valueOf(txn.getPoints()));
+            } else {
+                int points = payersPoints.get(txn.getPayer()) + txn.getPoints();
+                payersPoints.put(txn.getPayer(), Integer.valueOf(points));
+            }
+            transactions.add(txn);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return txn;
+    }
+
+    public List<SpentTransaction> spendPoints(Transaction pointsToSpend) {
+        this.transactions.sort((txn1, txn2) -> txn1.getDate().compareTo(txn2.getDate()));
+        List<Transaction> txnList = new ArrayList<>(this.transactions); //copy of transactions to avoid ConcurrentModificationException
+
+        List<SpentTransaction> spentTransactions = new ArrayList<>();
+        int spent = pointsToSpend.getPoints();
+        // loop through transactions, subtract points from payer's points
+        for (Transaction txn : txnList) {
+            if (spent > 0) {
+                if (spent - txn.getPoints() < 0) {
+                    spent = txn.getPoints() - spent;
+                    spentTransactions.add(new SpentTransaction(txn.getPayer(), spent * -1));
+                    this.payersPoints.put(txn.getPayer(), spent);
+                    txn.setPoints(spent);
+                    spent = 0;
+                } else {
+                    spent -= txn.getPoints();
+                    spentTransactions.add(new SpentTransaction(txn.getPayer(), txn.getPoints() * -1));
+                    this.payersPoints.put(txn.getPayer(), this.payersPoints.get(txn.getPayer()) - txn.getPoints());
+                    txn.setPoints(0);
+                }
+            } else {
+                break;
+            }
+        }
+
+        return spentTransactions;
+    }
+
+    public HashMap<String, Integer> findPayersPoints() {
+        return this.payersPoints;
+    }
+
+    public List<Transaction> findTransactions() {
+        return this.transactions;
+    }
+}
